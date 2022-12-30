1394db66ff8ebfe9ef210b6b11288b19f0141ff8
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2022-12-30 20:43:19 +0100

    add team detail

--- team/views.py
+++ team/views.py
@@ -6,3 +6,18 @@ from .models import team1
 def team_list(request):
     all = team1.objects.all()
     return render (request, 'team.html', {'data':all})
+
+
+def team_detail(request,id):
+    team = team1.objects.get(id=id)
+    return render (request, 'single.html', {'data':team})
+
+def create(request):
+    pass
+
+
+def edit(request):
+    pass
+
+def delete(request):
+    pass
