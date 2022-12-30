f6340f0f3f861941592f4a1d8ae88c959118101d
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2022-12-30 20:32:59 +0100

    add team list

--- mymarket/urls.py
+++ mymarket/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from team.views import team_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('team/', team_list)
 ]
 
 
--- team/views.py
+++ team/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import team1
 
 # Create your views here.
+
+def team_list(request):
+    all = team1.objects.all()
+    return render (request, 'team.html', {'data':all})
