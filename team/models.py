e46927c38cadccadea386474456c90b8bb74c71b
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2023-01-07 00:07:57 +0100

    add class category

--- team/models.py
+++ team/models.py
@@ -8,8 +8,19 @@ class team1(models.Model):
     content = models.TextField(max_length=5000)
     image = models.ImageField(upload_to='team/')
     author = models.ForeignKey(User, related_name='team_author', on_delete=models.CASCADE)
+    Category = models.ForeignKey('category', related_name='team_category', on_delete=models.CASCADE, null=True, blank=True)
 
 
 
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
+
+
+    def __str__(self):
+        return self.name
