fa1eb0f3f49ac228da04bbb401da9e4773d41dc9
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2023-01-07 01:12:37 +0100

    adding templats to the project

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from . import models
+admin.site.register(models.About)
+admin.site.register(models.Skills)
+admin.site.register(models.Education)
+admin.site.register(models.Experience)
+admin.site.register(models.Service)
+admin.site.register(models.Projects)
--- about/models.py
+++ about/models.py
@@ -11,6 +11,9 @@ class About(models.Model):
     age=models.IntegerField()
     gender=models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.name
+
 
 SKILL_TYPE =(
     ('Coding','Coding'),
@@ -22,12 +25,17 @@ class Skills(models.Model):
     percentage=models.IntegerField()
     type=models.CharField(max_length=15, choices=SKILL_TYPE)
 
+    def __str__(self):
+        return self.skill
+
 class Education(models.Model):
     year=models.CharField(max_length=50)
     title=models.CharField(max_length=100)
     place=models.CharField(max_length=50)
     description = models.CharField(max_length=300)
     last=models.BooleanField(default=False)
+    def __str__(self):
+        return self.title
 
 class Experience(models.Model):
     year=models.CharField(max_length=50)
@@ -35,16 +43,20 @@ class Experience(models.Model):
     place=models.CharField(max_length=50)
     description = models.CharField(max_length=300)
     ast=models.BooleanField(default=False)
+    def __str__(self):
+        return self.title
 
 
 class Service(models.Model):
     icon=models.CharField(max_length=20)
     title=models.CharField(max_length=100)
     description=models.CharField(max_length=300)
-
+    def __str__(self):
+            return self.title
 
 class Projects(models.Model):
     name = models.CharField(max_length=100)
     image=models.ImageField(upload_to='projects/')
     tags=models.CharField(max_length=100)
-
+    def __str__(self):
+        return self.name
