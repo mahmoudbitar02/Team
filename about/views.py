7207825bc7a5cac1139536fee703c99011c0adeb
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2023-01-09 22:58:11 +0100

    add Skills

--- about/views.py
+++ about/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
-from .models import About
+from .models import About, Skills
 
 
 # Create your views here.
 
 def home (request):
     about = About.objects.last()
+    coding_skills= Skills.objects.filter(type='Coding')
+    design_skills = Skills.objects.filter(type='Design')
 
     return render(request, 'index-2.html',{
-        'about':about
-    
+        'about':about,
+        'coding_skill':coding_skills,
+        'design_skill':design_skills
     
     })
