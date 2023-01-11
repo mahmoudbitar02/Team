5507e60b16f57a8e95f2267cbd0eb9183d1b8a60
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2023-01-11 23:49:57 +0100

    add service

--- about/views.py
+++ about/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import About, Skills, Education, Experience
-
+from .models import About, Skills, Education, Experience, Service, Projects
+from team.models import team1
 
 # Create your views here.
 
@@ -10,6 +10,9 @@ def home (request):
     design_skills = Skills.objects.filter(type='Design')
     education=Education.objects.all()
     experience=Experience.objects.all()
+    service= Service.objects.all()
+    project =Projects.objects.all()
+    team = team1.objects.all()
 
     return render(request, 'index-2.html',{
         'about':about,
@@ -17,6 +20,9 @@ def home (request):
         'design_skill':design_skills,
         'education':education,
         'experience':experience,
+        'service':service,
+        'project':project,
+        'team':team
 
     
     })
